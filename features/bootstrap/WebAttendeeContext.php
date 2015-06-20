95ebb068f0080175a87bbe3fd967b3d4df794907
ricfrank <riccardo.franconi@gmail.com>
2015-06-20 09:12:52 +0200

    remove empty code

--- features/bootstrap/WebAttendeeContext.php
+++ features/bootstrap/WebAttendeeContext.php
@@ -12,17 +12,6 @@ use Behat\Gherkin\Node\TableNode;
 class WebAttendeeContext implements Context, SnippetAcceptingContext
 {
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
-    public function __construct()
-    {
-    }
-
-    /**
      * @Given a conference named :arg1 with :arg2 track
      */
     public function aConferenceNamedWithTrack($arg1, $arg2)
@@ -48,27 +37,6 @@ class WebAttendeeContext implements Context, SnippetAcceptingContext
      * @Then the chosen talk for :arg1 slot of my schedule should be the :arg2
      */
     public function theChosenTalkForSlotOfMyScheduleShouldBeThe($arg1, $arg2)
-    {
-    }
-
-    /**
-     * @Given a conference named :arg1 with :arg2 tracks
-     */
-    public function aConferenceNamedWithTracks($arg1, $arg2)
-    {
-    }
-
-    /**
-     * @When I try to choose the :arg1 talk for my personal schedule of this conference
-     */
-    public function iTryToChooseTheTalkForMyPersonalScheduleOfThisConference($arg1)
-    {
-    }
-
-    /**
-     * @Then I should be told that slot is already taken by another talk  Scenarios in ubiquitous language
-     */
-    public function iShouldBeToldThatSlotIsAlreadyTakenByAnotherTalkScenariosInUbiquitousLanguage()
     {
     }
 }
