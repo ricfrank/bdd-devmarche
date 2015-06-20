b19209e85107724928345c767f8e4806472d7744
ricfrank <riccardo.franconi@gmail.com>
2015-06-20 09:12:52 +0200

    remove empty code

--- features/bootstrap/AttendeeContext.php
+++ features/bootstrap/AttendeeContext.php
@@ -22,17 +22,6 @@ class AttendeeContext implements Context, SnippetAcceptingContext
     private $mySchedule;
 
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
-    public function __construct()
-    {
-    }
-
-    /**
      * @Given a conference named :name with :count track
      */
     public function aConferenceNamedWithTrack($name, $count)
@@ -70,29 +59,5 @@ class AttendeeContext implements Context, SnippetAcceptingContext
         $talk = Talk::titled($talk);
         $slot = Slot::fromSchedule($slot);
         assertTrue($this->mySchedule->isTalkChoosenForSlot($talk, $slot));
-    }
-
-    /**
-     * @Given a conference named :arg1 with :arg2 tracks
-     */
-    public function aConferenceNamedWithTracks($arg1, $arg2)
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @When I try to choose the :arg1 talk for my personal schedule of this conference
-     */
-    public function iTryToChooseTheTalkForMyPersonalScheduleOfThisConference($arg1)
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Then I should be told that slot is already taken by another talk  Scenarios in ubiquitous language
-     */
-    public function iShouldBeToldThatSlotIsAlreadyTakenByAnotherTalkScenariosInUbiquitousLanguage()
-    {
-        throw new PendingException();
     }
 }
